0aa88d36aea5a9a0ee8c72de8d7ac3483a5b00fd
hxlei0827 <haoxianglei666@gmail.com>
2022-12-01 19:46:12 -0800

    Assignment4

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ https://hxlei0827.shinyapps.io/assignment-b3-hxlei0827/
 ### 2. add a checkbox by selecting the country "FRANCE"
 ### 3. Use the DT package to turn a static table into an interactive table
 ### Data source: https://github.com/daattali/shiny-server/tree/master/bcl/data
+
+
+# Assignment 4
+
+## Choose Option 3, continue working on BC Liquor app
+
+### 1. enable user to select a theme 
+### 2. enable user to select a country
+### 3. use tabsetPanel to make the mainPanel more intuitive 
--- app.R
+++ app.R
@@ -1,14 +1,18 @@
 library(shiny)
 library(tidyverse)
 library(DT)
+library(shinythemes)
 
 bcl <- read_csv("https://raw.githubusercontent.com/daattali/shiny-server/master/bcl/www/bcl-data.csv")
 
 #ggplot(bcl, aes(Alcohol_Content)) + geom_histogram()
 
 
-#feature1 : adding an image
+#A3_feature1 : adding an image
+#A4_feature1: adding a theme selection for the user
+#A4_feature2: adding selection of the country for the user
 ui <- fluidPage(
+  shinythemes::themeSelector(),
   titlePanel("BC Liquer Store Data"),
   h5("Welcome to my shiny app"),
   br(),
@@ -16,31 +20,37 @@ ui <- fluidPage(
   img(src = "bclimage.png"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("priceInput", "price", 0, 100, value = c(25, 40), pre = "$"),
+      sliderInput("priceInput", "price", 0, 100, value = c(0, 80), pre = "$"),
                  
-                 radioButtons("typeInput","Type", choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE")),
-      checkboxInput("France", "Only From France ", FALSE)),
+      radioButtons("typeInput","Type", choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE")),
+      
+      selectInput("countryInput", "Country", choices = bcl$Country)
+      ),
+#A4_feature3: make the main Panel more clear using tabsetPanel
+    
     mainPanel(
-      plotOutput("alcohol_hist"), 
-      DT::dataTableOutput("data_table"))
+      tabsetPanel(
+        tabPanel("Histogram", plotOutput("alcohol_hist")),
+        tabPanel("Data Table", DT::dataTableOutput("data_table"))
+                  )
+      
+      )
   ),
   a(href = "https://github.com/daattali/shiny-server/tree/master/bcl", "CLICK_HERE_For original dataset")
 )
 
-# feature 2: add a checkbox by selecting the country "FRANCE"
+# A3_feature 2: add a checkbox by selecting the country "FRANCE"
 server <- function(input, output) {
   filtered_data <- 
-    reactive(if (input$France){
+    reactive(if (is.null(input$countryInput)){
+      return(NULL)
+      
+    }
+    else{
       bcl %>% filter(Price > input$priceInput[1] & 
                        Price < input$priceInput[2] & 
                        Type == input$typeInput)%>%
-              filter(Country == "FRANCE")
-      
-    }
-    else{bcl %>% filter(Price > input$priceInput[1] & 
-                          Price < input$priceInput[2] & 
-                          Type == input$typeInput)
-
+        filter(Country == input$countryInput)
     })
   
   
@@ -50,7 +60,7 @@ server <- function(input, output) {
         ggplot(aes(Alcohol_Content)) + geom_histogram()
     })
   
-# feature 3: "Use the DT package to turn a static table into an interactive table."
+# A3_feature 3: "Use the DT package to turn a static table into an interactive table."
   output$data_table <- 
     DT::renderDataTable({
       filtered_data()
